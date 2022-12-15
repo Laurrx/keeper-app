6e017e0db49c9ac1f513e4988872c791807cda10
Laurrx <c.lauralex1@gmail.com>
2022-12-15 10:30:24 +0200

    latest changes

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import StickyNote2Icon from '@mui/icons-material/StickyNote2';
+
+function Header() {
+    return <header><h1><StickyNote2Icon />Keeper App</h1></header>
+}
+
+export default Header;
